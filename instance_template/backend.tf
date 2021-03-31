4370babdb70a007c945adffd8a19ef4a1188a72b
Yougo007 <33158785+Yougo007@users.noreply.github.com>
2021-03-31 18:48:04 -0400

    Update backend.tf

--- instance_template/backend.tf
+++ instance_template/backend.tf
@@ -15,7 +15,7 @@
 
 terraform {
   backend "gcs" {
-    bucket = "yougo-projects-tfstate"
-    prefix = "env/dev"
+    bucket = "yougo-projects-tfstate1"
+    prefix = "env/searchking-instance-template"
   }
 }
